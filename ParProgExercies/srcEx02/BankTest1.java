0424c0ca9c9c80c8b7420ec63c6c41fae7fd9bd9
TheoWinter <theo.winter@gmail.com>
2014-02-24 19:28:07 +0100

    Exercise 1 solved. No more race conditions.

--- ParProgExercies/srcEx02/BankTest1.java
+++ ParProgExercies/srcEx02/BankTest1.java
@@ -1,17 +1,18 @@
+import java.util.ArrayList;
+
 class BankAccount {
 	private int balance = 0;
 
 	public synchronized void deposit(int amount) throws InterruptedException {
-		balance += amount;
+			balance += amount;
+			notifyAll();
 	}
 
-	public synchronized boolean withdraw(int amount) throws InterruptedException {
-		if (amount <= balance) {
-			balance -= amount;
-			return true;
-		} else {
-			return false;
+	public synchronized void withdraw(int amount) throws InterruptedException {
+		while (amount > balance) {
+		      wait();
 		}
+		balance -= amount;
 	}
 
 	public int getBalance() {
@@ -32,6 +33,7 @@ class BankCustomer extends Thread {
 		for (int k = 0; k < NOF_TRANSACTIONS; k++) {
 			try {
 				account.deposit(100);
+		
 			} catch (InterruptedException anEx) {
 				// TODO Auto-generated catch block
 				anEx.printStackTrace();
@@ -51,8 +53,24 @@ public class BankTest1 {
 
 	public static void main(String[] args) {
 		BankAccount account = new BankAccount();
+		int balanceBeforeTransactions = account.getBalance();
+		ArrayList<BankCustomer> customerList = new ArrayList<BankCustomer>();
 		for (int i = 0; i < NOF_CUSTOMERS; i++) {
-			new BankCustomer(account).start();
+			BankCustomer bankMensch = new BankCustomer(account);
+			bankMensch.start();
+			customerList.add(bankMensch);
 		}
+		for (int i=0; i<customerList.size(); i++){
+			try {
+				customerList.get(i).join();
+			} catch (InterruptedException anEx) {
+				// TODO Auto-generated catch block
+				anEx.printStackTrace();
+			}
+		}
+		int balanceAfterTransactions = account.getBalance();
+		System.out.println("Balance before Transactions:"+balanceBeforeTransactions);
+		System.out.println("Balance after Transactions:"+balanceAfterTransactions);
+		System.out.println("Error through Race-Conditions:"+(balanceBeforeTransactions-balanceAfterTransactions));
 	}
 }
