1688d3f6611b3b8bbfac628e2289ae7bd4c08db3
TheoWinter <theo.winter@gmail.com>
2014-03-03 17:36:23 +0100

    Initial commit for exercises week3

---
+++ ParProgExercies/srcEx03/exercise01/WarehouseWithMonitor.java
@@ -0,0 +1,19 @@
+package exercise01;
+
+public class WarehouseWithMonitor implements Warehouse {
+  //TODO
+  
+  public WarehouseWithMonitor(int capacity) {
+    // TODO
+  }
+  
+  @Override
+  public void put(int amount) throws InterruptedException {
+    // TODO
+  }
+
+  @Override
+  public void get(int amount) throws InterruptedException {
+    // TODO
+  }
+}
---
+++ ParProgExercies/srcEx03/exercise02/FormulaOne.java
@@ -0,0 +1,7 @@
+package exercise02;
+
+public class FormulaOne {
+	public static void main(String[] args) throws InterruptedException {
+		new MonitorSyncedRaceControl().runRace();
+	}
+}
