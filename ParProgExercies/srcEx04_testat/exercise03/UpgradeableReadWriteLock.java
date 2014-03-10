f7bc08099a10cee246333f95fbd8e5ec13e1ab7a
TheoWinter <theo.winter@gmail.com>
2014-03-10 19:25:38 +0100

    I seem to have solved exercise 3 .. the provided unit-tests run through but somehow that seemed to be too easy.

--- ParProgExercies/srcEx04_testat/exercise03/UpgradeableReadWriteLock.java
+++ ParProgExercies/srcEx04_testat/exercise03/UpgradeableReadWriteLock.java
@@ -1,29 +1,40 @@
 package exercise03;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class UpgradeableReadWriteLock {
+	private ReadWriteLock rwLock = new ReentrantReadWriteLock(true);
 	// TODO: Implement
 
 	public void readLock() throws InterruptedException {
+		rwLock.readLock().lock();
 		// TODO:
+		
 	}
 
 	public void readUnlock() {
+		rwLock.readLock().unlock();
 		// TODO:
 	}
 
 	public void upgradeableReadLock() throws InterruptedException {
+		rwLock.writeLock().lock();
 		// TODO:
 	}
 
 	public void upgradeableReadUnlock() {
+		rwLock.writeLock().unlock();
 		// TODO:
 	}
 
 	public void writeLock() throws InterruptedException {
+		rwLock.writeLock().lock();
 		// TODO:
 	}
 
 	public void writeUnlock() {
+		rwLock.writeLock().unlock();
 		// TODO:
 	}
 }
