455995e89fa56d26a1c0a931d1b89ebd435d4540
TheoWinter <theo.winter@gmail.com>
2014-03-18 08:40:45 +0100

    changed quicksort method to be worked, but things don't work yet.

--- ParProgExercies/srcEx05/exercise01/QuickSortSample.java
+++ ParProgExercies/srcEx05/exercise01/QuickSortSample.java
@@ -1,37 +1,53 @@
 package exercise01;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class QuickSortSample {
 	private static final int NOF_ELEMENTS = 10_000_000;
-
-	// sorts the partition between array[left] and array[right]
-	private static void quickSort(int[] array, int left, int right) {
-		int i = left, j = right;
-		long m = array[(left + right) / 2];
-		do {
-			while (array[i] < m) {
-				i++;
-			}
-			while (array[j] > m) {
-				j--;
-			}
-			if (i <= j) {
-				int t = array[i];
-				array[i] = array[j];
-				array[j] = t;
-				i++;
-				j--;
-			}
-		} while (i <= j);
-		if (j > left) {
-			quickSort(array, left, j);
-		}
-		if (i < right) {
-			quickSort(array, i, right);
-		}
+	
+	public static void main(String[] args) {
+		int[] numberArray = createRandomArray(NOF_ELEMENTS);
+		//Setting up Thread-pool before string to measure:
+		ExecutorService threadPool = Executors.newFixedThreadPool(2);
+		Future<int[]> future1;
+		Future<int[]> future2;
+		
+		int[] firstHalf = Arrays.copyOfRange(numberArray, 0, numberArray.length/2);
+		int[] secondHalf = Arrays.copyOfRange(numberArray, numberArray.length/2, numberArray.length);
+		
+		future1 = threadPool.submit(new QuickSortWorker(firstHalf));
+		future2 = threadPool.submit(new QuickSortWorker(secondHalf));
+		
+		try {
+			int[] result1 = future1.get();
+			int[] result2 = future2.get();
+			checkSorted(result1);
+			checkSorted(result2);
+			System.out.println("good");
+		} catch (InterruptedException anEx) {
+			// TODO Auto-generated catch block
+			anEx.printStackTrace();
+		} catch (ExecutionException anEx) {
+			// TODO Auto-generated catch block
+			anEx.printStackTrace();
+		} 
+		
+		threadPool.shutdown();
+		
+		long startTime = System.currentTimeMillis();
+		
+		
+		long stopTime = System.currentTimeMillis();
+		System.out.println("Total time: " + (stopTime - startTime) + " ms");
+		checkSorted(numberArray);
 	}
-
+	
 	private static int[] createRandomArray(int length) {
 		Random random = new Random(4711);
 		int[] numberArray = new int[length];
@@ -48,13 +64,54 @@ public class QuickSortSample {
 			}
 		}
 	}
+	
+	static class QuickSortWorker implements Callable<int[]>{
+		int[] arrayToBeSorted;
 
-	public static void main(String[] args) {
-		int[] numberArray = createRandomArray(NOF_ELEMENTS);
-		long startTime = System.currentTimeMillis();
-		quickSort(numberArray, 0, numberArray.length - 1);
-		long stopTime = System.currentTimeMillis();
-		System.out.println("Total time: " + (stopTime - startTime) + " ms");
-		checkSorted(numberArray);
+		/**
+		 * Creates a new instance of this class.
+		 *
+		 * @param aArrayToBeSorted
+		 */
+		public QuickSortWorker(int[] aArrayToBeSorted) {
+			super();
+			arrayToBeSorted = aArrayToBeSorted;
+		}
+
+		// sorts the partition between array[left] and array[right]
+		private void quickSort(int[] array, int left, int right) {
+			int i = left, j = right;
+			long m = array[(left + right) / 2];
+			do {
+				while (array[i] < m) {
+					i++;
+				}
+				while (array[j] > m) {
+					j--;
+				}
+				if (i <= j) {
+					int t = array[i];
+					array[i] = array[j];
+					array[j] = t;
+					i++;
+					j--;
+				}
+			} while (i <= j);
+			if (j > left) {
+				quickSort(array, left, j);
+			}
+			if (i < right) {
+				quickSort(array, i, right);
+			}
+		}
+
+		/* (non-Javadoc)
+		 * @see java.util.concurrent.Callable#call()
+		 */
+		@Override
+		public int[] call() throws Exception {
+			quickSort(arrayToBeSorted, 0, arrayToBeSorted.length - 1);
+			return arrayToBeSorted;
+		}
 	}
 }
