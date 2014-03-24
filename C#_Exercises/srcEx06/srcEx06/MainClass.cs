beff7e4c1911c81005e40f8240b3d40d9d4c0f47
TheoWinter <theo.winter@gmail.com>
2014-03-24 18:07:19 +0100

    exercise 1 initial file commit

---
+++ C#_Exercises/srcEx06/srcEx06/MainClass.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace srcEx06
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("dddd");
+			Program_01 runProgram = new Program_01 ();
+			runProgram.run ();
+		}
+	}
+}
