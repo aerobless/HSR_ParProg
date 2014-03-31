7dfc3621ec32473c039e5fcc54798743368bfd28
TheoWinter <theo.winter@gmail.com>
2014-03-31 18:53:15 +0200

    Initial commit exercises 2 & 3 of week 7

---
+++ C#_Exercises/srcEx073/Pitfall2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pitfall2 {
+  class Program {
+    static void Main(string[] args) {
+      new Downloader().DownloadAsync().Wait();
+    }
+  }
+}
