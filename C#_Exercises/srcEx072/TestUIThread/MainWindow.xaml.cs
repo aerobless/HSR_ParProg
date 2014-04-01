e3c54a700a08b23e741bded13c76eba41a903e0f
Theo Winter <theo.winter@gmail.com>
2014-04-01 09:29:33 +0200

    Solved exercise 2a
    also added bin/obj directories to .gitignore. The correct way to specify
    path in windows git is:
    "C#_Exercises/srcEx072/TestUIThread/bin/"

--- C#_Exercises/srcEx072/TestUIThread/MainWindow.xaml.cs
+++ C#_Exercises/srcEx072/TestUIThread/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Windows;
+using System.Threading;
 
 namespace TestUIThread {
   public partial class MainWindow : Window {
@@ -13,13 +14,27 @@ namespace TestUIThread {
     private void startCalculationButton_Click(object sender, RoutedEventArgs e) {
       calculationResultLabel.Content = "(computing)";
       long number;
-      if (long.TryParse(numberTextBox.Text, out number)) {
-        if (_IsPrime(number)) {
-          calculationResultLabel.Content = "Prime";
-        } else {
-          calculationResultLabel.Content = "No prime";
-        }
-      }
+      if (long.TryParse(numberTextBox.Text, out number))
+        new Thread(() => {
+            String result = "";
+            {
+                if (_IsPrime(number))
+                {
+                    result = "Prime";
+                }
+                else
+                {
+                    result = "No prime";
+                }
+            }
+
+        Dispatcher.BeginInvoke(new ThreadStart(() => {
+
+            calculationResultLabel.Content = result;
+
+        }));
+
+      }).Start();
     }
 
     private bool _IsPrime(long number) {
