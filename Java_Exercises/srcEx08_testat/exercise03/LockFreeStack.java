cc70329a7ce3008fda8b4c720d39b2afb0eebc82
TheoWinter <theo.winter@gmail.com>
2014-04-08 09:31:23 +0200

    working on exercise 3, added node class and push method according to the slides of this week

--- Java_Exercises/srcEx08_testat/exercise03/LockFreeStack.java
+++ Java_Exercises/srcEx08_testat/exercise03/LockFreeStack.java
@@ -1,15 +1,39 @@
 package exercise03;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 // TODO: Implement lock-free stack
 public class LockFreeStack<T> implements Stack<T> {
+	AtomicReference<Node<T>> top = new AtomicReference<>();
+	
   // TODO: Design stack node class
+	private static class Node <E>{
+		public final E item;
+		public Node<E> next;
+		
+		public Node(E item) {
+			this.item = item;
+		}
+		
+		public void setNext(Node<E> item){
+			next = item;
+		}
+	}
   
   public void push(T value) {
     // TODO: Implement lock-free push operation
+	  Node<T> newNode = new Node<>(value);
+	  Node<T> current;
+	  do{
+		  current = top.get();
+		  newNode.setNext(current);
+	  } while (!top.compareAndSet(current, newNode));
   }
   
   public T pop() {
     // TODO: Implement lock-free push operation
     throw new RuntimeException("not implemented");
   }
+  
+  
 }
