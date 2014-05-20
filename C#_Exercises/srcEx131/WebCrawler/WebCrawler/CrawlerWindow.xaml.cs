37343364e682fa98a50a0998f716d1f6ec97008a
Theo Winter <theo@w1nter.com>
2014-05-20 09:32:32 +0200

    exercise week 13

---
+++ C#_Exercises/srcEx131/WebCrawler/WebCrawler/CrawlerWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Windows;
+
+namespace WebCrawler {
+  public partial class CrawlerWindow : Window {
+    private WebClient webClient = new WebClient();
+    private ReplaySubject<Uri> addresses = new ReplaySubject<Uri>();
+
+    public CrawlerWindow() {
+      InitializeComponent();
+      DefineWorkflow();
+    }
+
+    private void DefineWorkflow() {
+        // TODO: Define Rx workflow
+        // TODO: Display each result with resultListView.Items.Add()
+
+        (
+            from uri in addresses
+            from link in webClient.LinksInPage(uri)
+            where webClient.IsWebLink(link)
+            select link
+        ).Distinct().ObserveOnDispatcher()
+        .Subscribe((uri) => resultListView.Items.Add(uri));
+
+
+    }
+    private void startButton_Click(object sender, RoutedEventArgs e) {
+        // TODO: Push into Rx workflow
+        resultListView.Items.Clear();
+        addresses.OnNext(new Uri(addressTextBox.Text));
+    }
+  }
+}
